78efde677266c49d9508a738a3011f3323b8f67a
Jonatan HHallenberg <jonatan.hallenberg@sprinto.se>
2020-11-19 10:56:41 +0100

    first commit

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * Calculator
+ */
+
+ public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Vad vill du räkna ut?");
+        String calculation = System.console().readLine();
+        
+        if (calculation.indexOf('+') > -1) {
+            String[] splitCalc = calculation.split("\\+");
+            int sum = Integer.parseInt(splitCalc[0]) + Integer.parseInt(splitCalc[1]);
+            System.out.println("Svaret är: " + sum);
+        }
+
+        Person person = new Person();
+        System.out.println(person.x);
+        
+    }
+ }
---
+++ Person.java
@@ -0,0 +1,3 @@
+public class Person {
+    int x = 5;
+}
